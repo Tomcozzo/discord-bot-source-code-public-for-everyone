d3a340f7abfa7088b165158c7e5b7ea9aa887db5
Tomcozzo <thomascorran6@gmail.com>
2023-03-06 16:13:10 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const client = new Discord.Client({ 
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+
+]})
+
+const prefix = "!"
+
+client.on("messageCreate", message => {
+    if(message.content === prefix + "help") {
+        message.channel.send("this is coming soon!")
+    }
+})
+
+client.login(" your token here ")
